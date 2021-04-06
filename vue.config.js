245e0b33646fa1e0472843720f27c4570ca3eeca
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2021-04-06 14:57:40 +0200

    Commit #2
    Added vue.config.js and commented out axios

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    devServer: {
+        port: 3000,
+        proxy: {
+            '/rest': {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            },
+            '/auth' : {
+                target: 'http://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
+}
